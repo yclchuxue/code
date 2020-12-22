723bdef37d3843a9590ce2c8b60371ef2df99fae
yclchuxue <3136165538@qq.com>
2020-12-22 21:09:39 +0800

    12.22

---
+++ 反转2.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Listnode{
+    int val;
+    struct Listnode *next;
+}Listnode;
+
+void print(Listnode *head)
+{
+    Listnode *q = head;
+    for(;q;q = q->next)
+    {
+        printf("%d\t", q->val);
+    }
+}
+
+Listnode *fz(Listnode *head)
+{
+    Listnode* pre = NULL; 
+    while (head != NULL) 
+    { 
+        Listnode* next = head->next; 
+        head->next = pre; 
+        pre = head; 
+        head = next; 
+    } 
+    return pre;
+}
+
+int main()
+{
+    int n;
+    Listnode *head = NULL;
+    Listnode *b = NULL;
+
+    do
+    {
+        scanf("%d", &n);
+        if(n != -1)
+        {
+            Listnode *p = (Listnode*)malloc(sizeof(Listnode));
+            p->val = n;
+            p->next = NULL;
+            if(!head)
+            {
+                head = p;
+                b = p;
+            }
+            else
+            {
+                b->next = p;
+                b = b->next;
+            }
+        }
+    }while(n != -1);
+    print(head);
+    Listnode *p = fz(head);
+    printf("\n");
+    print(p);
+
+    return 0;
+}
