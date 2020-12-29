ff526bda65605c14df8027e2897d768f45c83d77
yclchuxue <3136165538@qq.com>
2020-12-29 20:05:08 +0800

    12.29

---
+++ 5-4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int a[10],book[10],n;
+
+void dfs(int step)
+{
+    if(step == n+1)
+    {
+        for(int i = 1;i<=n;i++)
+        {
+            printf("%d",a[i]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        for(int i = 1;i<=n;i++)
+        {
+            if(book[i] == 0)
+            {
+                a[i] = i;
+                book[i] = 1;
+                dfs(step + 1);
+                book[i] = 0;
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    scanf("%d",&n);
+    dfs(1);
+
+    return 0;
+}
