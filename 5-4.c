96e4a84406da63cb6e016b049d2040d86061123e
yclchuxue <3136165538@qq.com>
2021-01-01 22:40:58 +0800

    1.1

--- 5-4.c
+++ 5-4.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 int a[10],book[10],n;
 
-void dfs(int step)
+void dfs(int step,int start)
 {
-    if(step == n+1)
+    if(step == n)
     {
-        for(int i = 1;i<=n;i++)
+        for(int i = 1;i<n;i++)
         {
             printf("%d",a[i]);
         }
         printf("\n");
+        return ;
     }
     else
     {
-        for(int i = 1;i<=n;i++)
+        for(int i = start;i<n;i++)
         {
             if(book[i] == 0)
             {
                 a[i] = i;
                 book[i] = 1;
-                dfs(step + 1);
+                dfs(step + 1,start+1);
                 book[i] = 0;
             }
         }
@@ -30,7 +31,7 @@ void dfs(int step)
 int main()
 {
     scanf("%d",&n);
-    dfs(1);
+    dfs(1,1);
 
     return 0;
 }
