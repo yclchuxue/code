bbf5910b48e8ce2ee73899b74afa3c32751d7c24
yclchuxue <3136165538@qq.com>
2021-02-15 18:04:01 +0800

    更新

--- 练习1.c
+++ 练习1.c
@@ -1,27 +1,36 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int cmp (const void * a, const void * b)
+{
+   return ( *(int*)a - *(int*)b );
+}
+
+int maxNumberOfBalloons(char * text)
+{
+    int A[26] = {0};
+    int n = strlen(text)-1;
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d\n",('a'-text[i]));
+    }
+    qsort(A,26, sizeof(int),cmp);
+
+    return A[19];
+}
+
 int main()
 {
-	int m=0,j,i,t1;
-	char a[15];
-	scanf("%s",a);
-	m=strlen(a);
-	for(i=0;i<m-1;i++)
-		{
-			for(j=0;j<m-i-1;j++)
-			{
-				if(a[j]>a[j+1])
-				    {
-					    t1=a[j];
-				    	a[j]=a[j+1];
-				     	a[j+1]=t1;
-			    	}
-			}
-				
-		}
-		for(i=0;i<=m;i++)
-		{
-			printf("%c",a[i]);
-		}
-		return 0;
+    char str[100];
+    fgets(str,100,stdin);
+    int n = maxNumberOfBalloons(str);
+    printf("%d", n);
+
+    return 0;
 }
+
+
+
+
+nlaebolko
