b8d69f1927e8c7049e6c3ac7a842623a622a7805
yclchuxue <3136165538@qq.com>
2021-02-19 20:47:02 +0800

    likou-2.19

---
+++ 二分查找.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int search(int* nums, int numsSize, int target)
+{
+    
+}
+
+int main()
+{
+    int A[100] = {0},m,n;
+    printf("请输入数组大小和数组以及要查找的数：\n");
+    scanf("%d %d",&m,&n);
+    for(int i = 0; i < m;i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    int x = search(A,m,n);
+
+    printf("%d",x);
+
+    return 0;
+}
+
+
+int search(int* nums, int numsSize, int target){
+    /* 定义左右边界 */
+    int left = 0, right = numsSize - 1;
+    /* 定义搜索区间为闭区间[left,right] */
+    while (left <= right) {
+        /* 1: mid = (left + right) / 2
+         * 2: mid = left + (right - left) / 2
+         * 第2种可以防止(left + right)直接相加数值过大越界的问题
+         */
+        int mid = left + (right - left) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] == target) {
+            return mid;
+        }
+    }
+
+    return -1;
+}
