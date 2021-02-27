d2ad73b696b1fb2ae216119d6f19fa63d46fd3e3
yclchuxue <3136165538@qq.com>
2021-02-27 12:42:48 +0800

    likou

---
+++ 字符压缩.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+char* compressString(char* S)
+{
+    int size = strlen(S);
+    if (size <= 2) return S;
+    char* str = (char*)malloc(sizeof(char) * (2 * size));
+    str[0] = S[0];
+    int count = 1, index = 1;
+    for (int i = 1; i < size + 1; i++) 
+    {
+        if (S[i] == S[i - 1]) 
+        {
+            count++;
+        }
+        else 
+        {
+            int num = (int)log10(count) + index;
+            index = num;
+            while (count) 
+            {
+                str[num--] = count % 10 +'0';
+                count /= 10;
+            }
+            str[++index] = S[i];
+            count = 1;
+            ++index;
+        }
+    }
+    if (index >= size + 1) return S;
+    return str;
+}
+
+
+int main()
+{
+    char str[100];
+    fgets(str, 100,stdin);
+    char *p = compressString(str);
+
+}
