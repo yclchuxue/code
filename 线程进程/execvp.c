a8e9843231d81b85232b4af136807e5544d220b8
yclchuxue <3136165538@qq.com>
2021-05-14 13:11:44 +0800

    linuxC

---
+++ 线程进程/execvp.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<signal.h>
+#include<string.h>
+#include<unistd.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char* argv[] = {"ls", "-l", NULL}; // 构造 vector，注意argv[0] 是占用参数
+    int i = 0;
+    while(argv[i])
+    {
+        printf("%s\n", argv[i]);
+        i++;
+    }
+    if (execvp(argv[0], argv) == -1) 
+    {                                      // 替换代码段和数据段，并重新从 ls 入口点执行
+        perror("exec");
+        return 1;
+    }
+
+    return 0;  
+}
